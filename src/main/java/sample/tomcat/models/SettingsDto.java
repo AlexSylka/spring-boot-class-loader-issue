12b2318dfda090c181aee576855fc8a3713d9960
AlexSylka <sylkaalex@gmail.com>
2018-09-14 22:24:48 +0300

    sample project

--- src/main/java/sample/tomcat/models/SettingsDto.java
+++ src/main/java/sample/tomcat/models/SettingsDto.java
@@ -1,10 +1,6 @@
 package sample.tomcat.models;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class SettingsDto {
     private Boolean useSelfSignUp;
     private Boolean approve;
@@ -14,4 +10,71 @@ public class SettingsDto {
     private String  networkType;
     private Boolean flowEnabled;
     private String  upc;
+
+
+    public Boolean getUseSelfSignUp() {
+        return useSelfSignUp;
+    }
+
+    public void setUseSelfSignUp(Boolean useSelfSignUp) {
+        this.useSelfSignUp = useSelfSignUp;
+    }
+
+    public Boolean getApprove() {
+        return approve;
+    }
+
+    public void setApprove(Boolean approve) {
+        this.approve = approve;
+    }
+
+    public Boolean getVerifyData() {
+        return verifyData;
+    }
+
+    public void setVerifyData(Boolean verifyData) {
+        this.verifyData = verifyData;
+    }
+
+    public Boolean getCollectMid() {
+        return collectMid;
+    }
+
+    public void setCollectMid(Boolean collectMid) {
+        this.collectMid = collectMid;
+    }
+
+    public String getPartnerName() {
+        return partnerName;
+    }
+
+    public void setPartnerName(String partnerName) {
+        this.partnerName = partnerName;
+    }
+
+    public String getNetworkType() {
+        return networkType;
+    }
+
+    public void setNetworkType(String networkType) {
+        this.networkType = networkType;
+    }
+
+    public Boolean getFlowEnabled() {
+        return flowEnabled;
+    }
+
+    public void setFlowEnabled(Boolean flowEnabled) {
+        this.flowEnabled = flowEnabled;
+    }
+
+    public String getUpc() {
+        return upc;
+    }
+
+    public void setUpc(String upc) {
+        this.upc = upc;
+    }
+
+
 }
--- src/main/java/sample/tomcat/models/SettingsModel.java
+++ src/main/java/sample/tomcat/models/SettingsModel.java
@@ -1,17 +1,66 @@
 package sample.tomcat.models;
 
-import lombok.Builder;
-import lombok.Getter;
 
-@Getter
-@Builder
 public class SettingsModel {
-    public final Boolean useSelfSignUp;
-    public final Boolean approve;
-    public final Boolean verifyData;
-    public final Boolean collectMid;
-    public final Boolean flowEnabled;
-    public final String  partnerName;
-    public final String  networkType;
-    public final String upc;
+    private  Boolean useSelfSignUp;
+    private  Boolean approve;
+    private  Boolean verifyData;
+    private  Boolean collectMid;
+    private  Boolean flowEnabled;
+    private  String  partnerName;
+    private  String  networkType;
+    private  String upc;
+
+    SettingsModel(Boolean useSelfSignUp,
+                         Boolean approve,
+                         Boolean verifyData,
+                         Boolean collectMid,
+                         Boolean flowEnabled,
+                         String partnerName,
+                         String networkType,
+                         String upc) {
+        this.useSelfSignUp = useSelfSignUp;
+        this.approve = approve;
+        this.verifyData = verifyData;
+        this.collectMid = collectMid;
+        this.flowEnabled = flowEnabled;
+        this.partnerName = partnerName;
+        this.networkType = networkType;
+        this.upc = upc;
+    }
+
+
+    public Boolean getUseSelfSignUp() {
+        return useSelfSignUp;
+    }
+
+    public Boolean getApprove() {
+        return approve;
+    }
+
+    public Boolean getVerifyData() {
+        return verifyData;
+    }
+
+    public Boolean getCollectMid() {
+        return collectMid;
+    }
+
+    public Boolean getFlowEnabled() {
+        return flowEnabled;
+    }
+
+    public String getPartnerName() {
+        return partnerName;
+    }
+
+    public String getNetworkType() {
+        return networkType;
+    }
+
+    public String getUpc() {
+        return upc;
+    }
+
+
 }
